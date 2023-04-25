4bf6f0f80b27f383f31de1cbe25881690b5b516d
Greg Doud <gpdoud@gmail.com>
2023-04-25 09:22:48 -0400

    completed order change

--- src/app/order/order-change/order-change.component.ts
+++ src/app/order/order-change/order-change.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Order } from '../order.class';
+import { OrderService } from '../order.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Customer } from 'src/app/customer/customer.class';
+import { CustomerService } from 'src/app/customer/customer.service';
 
 @Component({
   selector: 'app-order-change',
@@ -6,5 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-change.component.css']
 })
 export class OrderChangeComponent {
+
+  pageTitle = "Order Change"
+  order!: Order;
+  customers!: Customer[];
+
+  constructor(
+    private ordSvc: OrderService,
+    private custSvc: CustomerService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  save(): void {
+    this.order.customerId = +this.order.customerId;
+    console.debug("B4:", this.order);
+    this.ordSvc.change(this.order).subscribe({
+      next: (res) => {
+        console.debug("Order Changed!");
+        this.router.navigateByUrl("/order/list");
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.custSvc.list().subscribe({
+      next: (res) => {
+        console.debug("Customers:", res);
+        this.customers = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+    let id = this.route.snapshot.params["id"];
+    this.ordSvc.get(id).subscribe({
+      next: (res) => {
+        console.debug("Order:", res);
+        this.order = res;
+        this.order.date = this.order.date.substring(0, 10);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 
 }
